c7863da7dd88a3818e0d0bab24b130e685cc3401
nemanja13 <nemanja.maksimovic.4.18@ict.edu.rs>
2024-03-03 12:40:08 +0100

    Backend - Implementation layer adding EF implementation of Create, Update and Delete (Ingredients, Recipes)

---
+++ CocktailRecipesApi/Implementation/Commands/IngredientCommands/EFUpdateIngredientCommand.cs
@@ -0,0 +1,51 @@
+﻿using Application.Commands.IngredientCommands;
+using Application.DataTransfer;
+using Application.Exceptions;
+using AutoMapper;
+using DataAccess;
+using Domain;
+using FluentValidation;
+using Implementation.Validators;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Implementation.Commands.IngredientCommands
+{
+    public class EFUpdateIngredientCommand : IUpdateIngredientCommand
+    {
+
+        private readonly CocktailRecipesContext _context;
+        private readonly UpdateIngredientValidator _validator;
+        private readonly IMapper _mapper;
+
+        public EFUpdateIngredientCommand(CocktailRecipesContext context, UpdateIngredientValidator validator, IMapper mapper)
+        {
+            _context = context;
+            _validator = validator;
+            _mapper = mapper;
+        }
+
+        public int Id => 2;
+
+        public string Name => "Updating Ingredient using Entity Framework";
+
+        public void Execute(IngredientDto request)
+        {
+            var ingredient = _context.Ingredients.FirstOrDefault(c => c.Id == request.Id);
+
+            if (ingredient == null)
+            {
+                throw new EntityNotFoundException(request.Id.Value, typeof(Ingredient));
+            }
+
+            _validator.ValidateAndThrow(request);
+
+            _mapper.Map(request, ingredient);
+
+            _context.SaveChanges();
+        }
+    }
+}
---
+++ CocktailRecipesApi/Implementation/Commands/RecipeCommands/EFDeleteRecipeCommand.cs
@@ -0,0 +1,40 @@
+﻿using Application.Commands.RecipeCommands;
+using Application.Exceptions;
+using DataAccess;
+using Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Implementation.Commands.RecipeCommands
+{
+    public class EFDeleteRecipeCommand : IDeleteRecipeCommand
+    {
+        private readonly CocktailRecipesContext _context;
+
+        public EFDeleteRecipeCommand(CocktailRecipesContext context)
+        {
+            _context = context;
+        }
+
+        public int Id => 6;
+
+        public string Name => "Deleting Cocktail recipe using Entity Framework";
+
+        public void Execute(int request)
+        {
+            var cocktailRecipe = _context.CocktailRecipes.Find(request);
+
+            if (cocktailRecipe == null)
+            {
+                throw new EntityNotFoundException(request, typeof(CocktailRecipe));
+            }
+
+            cocktailRecipe.DeleteDate = DateTime.UtcNow;
+
+            _context.SaveChanges();
+        }
+    }
+}
