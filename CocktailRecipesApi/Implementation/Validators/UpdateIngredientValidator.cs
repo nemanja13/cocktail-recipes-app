fd5eaa3ae52730bd56d58cdda555d685962b2b8d
nemanja13 <nemanja.maksimovic.4.18@ict.edu.rs>
2024-03-03 12:12:21 +0100

    Backend - adding Implementation layer (Adding Fluent Validations and String extension for password)

---
+++ CocktailRecipesApi/Implementation/Validators/UpdateIngredientValidator.cs
@@ -0,0 +1,26 @@
+﻿using Application.DataTransfer;
+using DataAccess;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Implementation.Validators
+{
+    public class UpdateIngredientValidator : AbstractValidator<IngredientDto>
+    {
+        public UpdateIngredientValidator(CocktailRecipesContext context)
+        {
+            RuleFor(r => r.Name).NotEmpty()
+                .WithMessage("Ingredient {PropertyName} must not be empty.").
+                DependentRules(() =>
+                {
+                    RuleFor(r => r.Name)
+                    .Must((dto, name) => !context.Ingredients.Any(c => c.Name == name && c.Id != dto.Id))
+                    .WithMessage(dto => $"Ingredient with name {dto.Name} already exists in database.");
+                });
+        }
+    }
+}
---
+++ CocktailRecipesApi/Implementation/Validators/UserLoginRequestValidator.cs
@@ -0,0 +1,35 @@
+﻿using Application.DataTransfer;
+using DataAccess;
+using FluentValidation;
+using Implementation.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Implementation.Validators
+{
+    public class UserLoginRequestValidator : AbstractValidator<UserLoginRequest>
+    {
+        public UserLoginRequestValidator(CocktailRecipesContext context)
+        {
+            RuleFor(x => x.Username)
+               .NotEmpty()
+               .WithMessage("Username must not be empty")
+               .DependentRules(() => {
+                   RuleFor(x => x.Username)
+                   .Must(username => context.Users.Any(u => u.Username == username))
+                   .WithMessage("User with an username of {PropertyValue} does not exist in database");
+               });
+            RuleFor(x => x.Password)
+               .NotEmpty()
+               .WithMessage("Password must not be empty")
+               .DependentRules(() => {
+                   RuleFor(x => x.Password)
+                   .Must(password => context.Users.Any(u => u.Password == password.MD5Encrypt()))
+                   .WithMessage("User with this password does not exist in database");
+               });
+        }
+    }
+}
