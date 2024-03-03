e3618bb5486ac8cce7b95696deb699f9475f69bd
nemanja13 <nemanja.maksimovic.4.18@ict.edu.rs>
2024-03-03 11:34:03 +0100

    Backend - adding Application layer (Dtos and base Interfaces)

---
+++ CocktailRecipesApi/Application/DataTransfer/RecipeDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.DataTransfer
+{
+    public class RecipeDto : DtoBase
+    {
+        public string Name { get; set; }
+        public string Image { get; set; }
+        public string Instructions { get; set; }
+        public int? TypeId { get; set; }
+        public string? Type { get; set; }
+        public int? MeasureId { get; set; }
+        public string? Measure { get; set; }
+        public IEnumerable<int> IngredientIds { get; set; } = new List<int>();
+        public IEnumerable<string> Ingredients { get; set; } = new List<string>();
+    }
+}
---
+++ CocktailRecipesApi/Application/UseCaseExecutor.cs
@@ -0,0 +1,40 @@
+﻿using Application.Exceptions;
+using Application.Queries;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application
+{
+    public class UseCaseExecutor
+    {
+        private readonly IApplicationActor actor;
+
+        public UseCaseExecutor(IApplicationActor actor)
+        {
+            this.actor = actor;
+        }
+
+        public TResult ExecuteQuery<TSearch, TResult>(IQuery<TSearch, TResult> query, TSearch search)
+        {
+            if (!actor.AllowedUseCases.Contains(query.Id))
+            {
+                throw new UnauthorizedUseCaseException(query, actor);
+            }
+
+            return query.Execute(search);
+        }
+
+        public void ExecuteCommand<TRequest>(ICommand<TRequest> command, TRequest request)
+        {
+            if (!actor.AllowedUseCases.Contains(command.Id))
+            {
+                throw new UnauthorizedUseCaseException(command, actor);
+            }
+
+            command.Execute(request);
+        }
+    }
+}
