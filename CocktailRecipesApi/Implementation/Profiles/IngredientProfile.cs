a797672eab95d83503f06930943eeed99e4935d2
nemanja13 <nemanja.maksimovic.4.18@ict.edu.rs>
2024-03-03 12:20:03 +0100

    Backend - adding Implementation layer (Adding AutoMapper and Profiles for AutoMapper)

---
+++ CocktailRecipesApi/Implementation/Profiles/IngredientProfile.cs
@@ -0,0 +1,20 @@
+﻿using Application.DataTransfer;
+using AutoMapper;
+using Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Implementation.Profiles
+{
+    public class IngredientProfile : Profile
+    {
+        public IngredientProfile()
+        {
+            CreateMap<Ingredient, IngredientDto>();
+            CreateMap<IngredientDto, Ingredient>();
+        }
+    }
+}
